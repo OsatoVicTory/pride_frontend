1968a0b9f36d83db8a98c220eaa760540b991ec6
Tory <76063134+OsatoVicTory@users.noreply.github.com>
2024-01-01 04:32:40 +0100

    Update WalletServices.js

--- src/Services/WalletServices/WalletServices.js
+++ src/Services/WalletServices/WalletServices.js
@@ -4,7 +4,7 @@ const options = {
     withCredentials: true
 };
 
-const SERVER = "https://pride-app.herokuapp.com";
+const SERVER = "https://pride-app.onrender.com";
 
 export const setUpWallet = async (data) => {
     return await axios.post(`${SERVER}/wallet`, data, options);
